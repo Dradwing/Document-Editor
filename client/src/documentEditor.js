31cedf9cda8433c1d77bac7ef8ce62734bcd29ed
Dradwing <Rushbuket@gmail.com>
2024-01-23 22:56:10 +0530

    fixing socket.io port

--- client/src/documentEditor.js
+++ client/src/documentEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor(props) {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io(":3001")
+    const s = io(`:${process.env.PORT}`)
     setSocket(s)
 
     return () => {
